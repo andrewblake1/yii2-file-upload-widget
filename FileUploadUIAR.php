bb3ea98b8f4bc5aff0c3d826d75ac59c62249f8b
Andrew Blake <admin@newzealandfishing.com>
2014-09-30 12:54:07 +1300

    Nearly there, about to look at cloud storage e.g. s3

--- FileUploadUIAR.php
+++ FileUploadUIAR.php
@@ -130,10 +130,9 @@ class FileUploadUIAR extends FileUpload
 				$(this).fileupload('option', 'done')
 					.call(this, $.Event('done'), {result: result});
 			});
-			
+
 			// set call back for when upload process finished
-			$('#$id').bind('fileuploadfinished', function (e, data)
-			{
+			$('#$id').bind('fileuploadfinished', function (e, data) {
 				// allow redirect only if no errors in form data
 				if(data.result.hasOwnProperty('redirect')) {
 					window.location.href = data.result.redirect;
@@ -145,19 +144,62 @@ class FileUploadUIAR extends FileUpload
 					$('#$id').yiiActiveForm('updateInputs', data.result.activeformerrors);
 				}
 			});
+
 			
-			// make the submit button work even without new files
+			// allow submit even without new files - which jquery file upload blocks by checking for file before sumitting
 			$('#$id .fileupload-buttonbar .start').on('click',function () { 
 				// allow update without having to upload - this courtesy of plugin author
 				var form = $('form').first();
 				if (!form.find('.files .start').length) {
 					// submit the normal ActiveForm way - no files being uploaded i.e. just the form data
-					$('#$id .btn.btn-primary.hide').click();
+					$('#$id').submit();
+				}
+			});
+			
+			// set call back for when upload process done - to block removal of the file input in case of error
+			// basically we do want to show file upload errors but return others to there pre-upload state
+			$('#$id').bind('fileuploaddone', function (e, data) {
+				// if there are errors there will be no redirect key in our json response from the UploadHandler
+				if(!data.result.hasOwnProperty('redirect')) {
+					// prevent the normal processing which will remove the file inputs for good. We want to keep them and put them back
+					// to a state where the good ones can be reused without the user having to re-select
+					e.preventDefault();
+			
+					// loop thru each of the rows in our fileupload widget
+					$('tr.template-upload.fade.in').each(function(index) {
+						var file = data.result.files[index];
+						var error;
+						// if upload was successful for this file
+						if(file) {
+			
+							// if there was an error returned from the server
+							if(file.hasOwnProperty('error')) {
+								// remove the progress bar
+								$('.progress', this).remove();
+								// set an error for display
+								error = file.error;
+							}
+							else {
+								// reset the progress bar
+								$('.progress-bar.progress-bar-success', $('.progress', this).attr('aria-valuenow', 0)).attr('style', 'width: 0%;');
+								// enable the start button - even though it is hidden
+								$('button.btn.btn-primary.start', this).prop('disabled', false);
+							}
+						}
+						else {
+							error = 'Empty file upload result';
+						}
+						
+						if(error) {
+							// display error
+							$('.error.text-danger', this).html(error);
+						}
+					});
 				}
 			});
 
 HERE;
-		
+
         $view->registerJs($js);
     }
 } 
