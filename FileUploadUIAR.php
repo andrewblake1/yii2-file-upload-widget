b481786d7de9a6696022ada30403a6adffa7ccc1
Andrew Blake <admin@newzealandfishing.com>
2014-10-03 10:29:52 +1300

    Moving towards s3 - selecting and sending working
    selecting for attributes and model working - moving towards s3 - need
    server to generate correct response so may as well start using
    dosamigos resource manager rather than continuing to work with
    BlueImp’s UploadHandler which has multi file multi input handling
    issues. Shifting to use 2amigos/yii2-resource-manager-component for
    amazon s3 as per example by tonydspaniard on
    https://github.com/2amigos/yii2-file-upload-widget/issues/7

--- FileUploadUIAR.php
+++ FileUploadUIAR.php
@@ -9,6 +9,7 @@ namespace dosamigos\fileupload;
 use dosamigos\gallery\GalleryAsset;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
+use yii\web\View;
 
 /**
  * FileUploadUI
@@ -89,49 +90,56 @@ class FileUploadUIAR extends FileUploadUI
 		$fileUploadTarget = "[name=\"{$this->name}\"]";
 		FileUploadUIAsset::register($view);
 
-        $options = Json::encode($this->clientOptions);
-		// set up for uploading
-        $view->registerJs(";jQuery('$fileUploadTarget').fileupload($options);");
+		// this from http://stackoverflow.com/questions/19807361/uploading-multiple-files-asynchronously-by-blueimp-jquery-fileupload
+		// to stop seperate requests for each file, split into our various suitable blocks
+		// once in doc ready
+		$view->registerJs(';var filesList = [], paramNames = [], elem = $("form");');
 		
-		$js = <<<HERE
+		// per target in doc ready
+        $options = Json::encode($this->clientOptions);
+        $view->registerJs(";$('$fileUploadTarget').fileupload($options);", View::POS_READY, $fileUploadTarget);
+		
+		// once on window load - using window load as the fileupload plugin needs ataching to target elements first before this code will work
+		$jsLoad = <<<HERE
+			// save reference to the file in a global array so that we can access later to send
+			$('input[type="file"]').on("fileuploadadd", function(e, data){
+				filesList.push(data.files[0]);
+				paramNames.push(e.delegatedEvent.target.name);
+			});
+
+			// block submit thru file upload - will happen by direct call above - even though would have though taking over click would have
+			// done this it does so guiessing send calls submit first or something and perhaps empties the q
+			$('input[type="file"]').bind('fileuploadsubmit', function (e, data) {
+				e.preventDefault();
+			});
 
 			// because we want to potentially return different arrays of files for different attributes we have different names for the file
 			// input and this is the array key we want - not just 'files' as the default plugin implementation assumes
-			jQuery('$fileUploadTarget').fileupload(
+			// also note that this should only attach to our model level button which means might need a hidden one if only attribute uploads
+			// could perhaps attach to the save button instead - i.e this fileupload attach to save rather than the file upload input as save will
+			// be there only once for the whole view if any attributes or model allows files
+// TODO need to iterate and deal with all files somehow and update all inputs - may need to trigger done or something against each input?
+			$('input[name="files[]"]').fileupload(
 				'option',
 				'getFilesFromResponse',
 				function (data) {
-					if (data.result && $.isArray(data.result['{$this->name}'])) {
-						return data.result['{$this->name}'];
+					if (data.result && $.isArray(data.result['files[]'])) {
+						return data.result['files[]'];
 					}
 					return [];
 				}
 			);
 			
-			// this from http://stackoverflow.com/questions/19807361/uploading-multiple-files-asynchronously-by-blueimp-jquery-fileupload
-			// to stop seperate requests for each file
-			var filesList = [], paramNames = [], elem = $("form");
-			file_upload = $('$fileUploadTarget').on("fileuploadadd", function(e, data){
-				filesList.push(data.files[0]);
-				paramNames.push(e.delegatedEvent.target.name);
-			});
-
 			// deal with click events ourselves on the main save button - there is also a hidden save button if no files
 			$('.fileupload-buttonbar .start').on('click',function () { 
 				// if there are some files in our upload q
 				if(filesList.length) {
 					// send them programatically
-					file_upload.fileupload('send', {files:filesList, paramName: paramNames});
+					$('[name="files[]"]').fileupload('send', {files:filesList, paramName: paramNames});
 				} else {
 					// TODO try just a submit thru yiiActiveForm plugin - might not noeed the button and the click
 					$('#activFormSave').click();
 				}
-			});
-
-			// block submit thru file upload - will happen by direct call above - even though would have though taking over click would have
-			// done this it does so guiessing send calls submit first or something and perhaps empties the q
-			$('$fileUploadTarget').bind('fileuploadsubmit', function (e, data) {
-				e.preventDefault();
 			});
 
 //need to change to our new targets							
@@ -154,7 +162,7 @@ class FileUploadUIAR extends FileUploadUI
 
 			// set call back for when upload process done - to block removal of the file input in case of error
 			// basically we do want to show file upload errors but return others to there pre-upload state
-			$('$fileUploadTarget').bind('fileuploaddone', function (e, data) {
+			$('[name="files[]"]').bind('fileuploaddone', function (e, data) {
 				e.preventDefault();
 				// if there are errors there will be no redirect member in our json response from the UploadHandler
 				// allow redirect only if no errors in form data
@@ -164,7 +172,7 @@ class FileUploadUIAR extends FileUploadUI
 				else {
 					// loop thru each of the rows in our fileupload widget
 					$('tr.template-upload.fade.in').each(function(index) {
-						var file = data.result.hasOwnProperty('{$this->name}') ? data.result['{$this->name}'][index] : null;
+						var file = data.result.hasOwnProperty('files[]') ? data.result['files[]'][index] : null;
 						var error;
 						// if an error was returned from the server
 						if(file && file.hasOwnProperty('error')) {
@@ -204,6 +212,7 @@ class FileUploadUIAR extends FileUploadUI
 HERE;
 		
 		// this needs to come after the fileUpload attachement to the file inputs which are in doc ready
-        $view->registerJs($js, \yii\web\View::POS_LOAD);
+		// the key makes this only once overall for the page/model
+        $view->registerJs($jsLoad, View::POS_LOAD, 'files[]');
     }
 } 
